a492a921b65ce411c08bd70f84da917449fbaeec
oxo <samuelesantonicola@gmail.com>
2024-07-15 05:53:10 +0200

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,3 @@
-<template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-    <ThreeScene />
-  </div>
-</template>
 
 <script>
 import ThreeScene from './components/ThreeScene.vue'
