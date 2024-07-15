af35ac432f6db9b6432046772782947ff3546acd
oxo <samuelesantonicola@gmail.com>
2024-07-15 05:34:45 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
-import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.128.0/build/three.module.js';
-import { EffectComposer } from 'https://cdn.jsdelivr.net/npm/three@0.128.0/examples/jsm/postprocessing/EffectComposer.js';
-import { RenderPass } from 'https://cdn.jsdelivr.net/npm/three@0.128.0/examples/jsm/postprocessing/RenderPass.js';
-import { ShaderPass } from 'https://cdn.jsdelivr.net/npm/three@0.128.0/examples/jsm/postprocessing/ShaderPass.js';
-import { VignetteShader } from 'https://cdn.jsdelivr.net/npm/three@0.128.0/examples/jsm/shaders/VignetteShader.js';
-
+import * as THREE from './node_modules/three/build/three.module.js';
+import { EffectComposer } from './node_modules/three/examples/jsm/postprocessing/EffectComposer.js';
+import { RenderPass } from './node_modules/three/examples/jsm/postprocessing/RenderPass.js';
+import { ShaderPass } from './node_modules/three/examples/jsm/postprocessing/ShaderPass.js';
+import { VignetteShader } from './node_modules/three/examples/jsm/shaders/VignetteShader.js';
 // Scene setup
 const scene = new THREE.Scene();
 scene.background = new THREE.Color(0x000000);
