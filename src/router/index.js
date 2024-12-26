71074176b6172367a814fc7357d578b19a30e375
oxo <samuelesantonicola@gmail.com>
2024-12-26 07:26:58 +0100

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,21 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Home from '../views/Home.vue'
+import ThreeScene from '../components/ThreeScene.vue'
 
 const routes = [
   {
     path: '/',
     name: 'Home',
-    component: Home
+    component: ThreeScene
   },
   {
     path: '/releases',
-    beforeEnter() {
-      window.location.href = 'https://distrokid.com/dashboard/album/?albumuuid=6CD90D9D-3A93-49E9-81A0EA277C878265'; // Replace with your actual releases URL
+    redirect: () => {
+      window.location.href = 'https://distrokid.com/hyperfollow/okiso/thunder-feat-hatsune-miku'
     }
   }
 ]
 
-const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
+export default createRouter({
+  history: createWebHistory('/site/'),
   routes
 })
-
-export default router
