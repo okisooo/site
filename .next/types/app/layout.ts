de49d14754c2aa8e18e790234252186448ab012f
oxo <samuelesantonicola@gmail.com>
2025-02-19 21:04:32 +0100

    Update package.json
    Update .gitignore
    
    Update deploy.yml
    
    yo
    
    yo
    
    Update package.json
    
    yo

--- .next/types/app/layout.ts
+++ .next/types/app/layout.ts
@@ -1,4 +1,4 @@
-// File: C:\Users\samue\Documents\GitHub\site\okiso-site\src\app\layout.tsx
+// File: C:\Users\samue\Documents\GitHub\site\src\app\layout.tsx
 import * as entry from '../../../src/app/layout.js'
 import type { ResolvingMetadata, ResolvingViewport } from 'next/dist/lib/metadata/types/metadata-interface.js'
 
--- .next/types/app/page.ts
+++ .next/types/app/page.ts
@@ -1,4 +1,4 @@
-// File: C:\Users\samue\Documents\GitHub\site\okiso-site\src\app\page.tsx
+// File: C:\Users\samue\Documents\GitHub\site\src\app\page.tsx
 import * as entry from '../../../src/app/page.js'
 import type { ResolvingMetadata, ResolvingViewport } from 'next/dist/lib/metadata/types/metadata-interface.js'
 
