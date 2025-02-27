4b79e406a9bfae75b18dee40646de7ec2b938249
oxo <samuelesantonicola@gmail.com>
2025-02-27 04:03:54 +0100

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -73,7 +73,7 @@ export default function Home() {
         <div className="px-6 py-3 relative">
           <Dock
             items={dockItems}
-            panelHeight={isMobile ? 50 : 68}
+            panelHeight={isMobile ? 59 : 68}
             baseItemSize={isMobile ? 40 : 50}
             magnification={isMobile ? 50 : 70}
             spring={{ mass: 0.2, stiffness: 200, damping: 18 }}
@@ -83,3 +83,4 @@ export default function Home() {
       </div>
     </div>
   );
+}
