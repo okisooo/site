d2f53f86019efd32c44be9310a2c06ae4979542c
oxo <samuelesantonicola@gmail.com>
2025-08-02 18:19:03 +0200

    yo

--- src/app/releases/page-server.tsx
+++
@@ -1,9 +0,0 @@
-import { fetchSpotifyReleases } from '@/lib/fetchSpotifyReleases';
-import ReleasesPageClient from './ReleasesPageClient';
-
-export default async function ReleasesPage() {
-    // Fetch releases at build time (server-side)
-    const releases = await fetchSpotifyReleases();
-
-    return <ReleasesPageClient releases={releases} />;
-}
